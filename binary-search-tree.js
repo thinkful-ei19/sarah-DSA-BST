b59f5c362731be7676ce6e89e69a35edc64aa099
ssheaks <sarah.sheaks@gmail.com>
2018-05-22 09:49:38 -0700

    create height of BST function

---
+++ binary-search-tree.js
@@ -0,0 +1,136 @@
+'use strict';
+
+class BinarySearchTree {
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    //if the tree is empty then this key being inserted is the root node of the tree
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {  
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (this.key == key) {
+      return this.value;
+    } else if (key < this.key && this.left) {
+      return this.left.find(key);
+    } else if (key > this.key && this.right) {
+      return this.right.find(key);
+    } else {
+      throw new Error('Key Error');
+    }
+  }
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      } else if (this.left) {
+        this._replaceWith(this.left);
+      } else if (this.right) {
+        this._replaceWith(this.right);
+      } else {
+        this._replaceWith(null);
+      }
+    } else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    } else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      } else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    } else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      } else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+}
+
+function heightBST(BST) {
+  if (BST === null) {
+    return 0;
+  } else if (!BST.left && !BST.right) {
+    return 1;
+  } else if (BST.left || BST.right) {
+    // console.log(Math.max(heightBST(BST.left), heightBST(BST.right) + 1));
+    return Math.max(heightBST(BST.left), heightBST(BST.right)) + 1;
+  }
+}
+
+// is it BST?
+// Write an algorithm to check whether an arbitrary binary tree is a binary search tree, assuming the tree does not contain duplicates
+
+
+function main() {
+  let BST = new BinarySearchTree();
+  BST.insert(3);
+  BST.insert(1);
+  BST.insert(4);
+  BST.insert(6);
+  BST.insert(9);
+  BST.insert(2);
+  BST.insert(5);
+  BST.insert(7);
+  // console.log(BST);
+  console.log(heightBST(BST));
+}
+
+main();
+
+module.exports = BinarySearchTree;
+
+// Create a BST class
+// Walk through the Binary Search Tree code in the curriculum and understand it well. Then write a Binary Search Tree class and with its core functions (insert, remove, find) from scratch.
+
+// Create a Binary Search Tree called BST and insert 3,1,4,6,9,2,5,7 to your tree. Compare your result with the result from the first exercise
---
+++ height-BST.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const BinarySearchTree = require('./binary-search-tree');
+
+// Height of a BST
+// Write an algorithm to find the height of a binary search tree. What is the run time of your algorithm?
+
+//height will equal level + 1
+//start at root node if no root node height = 0; if no children height = 1
+//else traverse left keep current count until left & right are null (store as max count)
+//go back up until we reach parent node with right branch then traverse down the left and track current count until all null (compare to max count)
+
+function heightBST(BST) {
+  if (BST === null) {
+    return 0;
+  } else if (!BST.left && !BST.right) {
+    return 1;
+  } else if (BST.left || BST.right) {
+    return Math.max(heightBST(BST.left), heightBST(BST.right) + 1);
+  }
+}
+
+console.log(heightBST(BST));
